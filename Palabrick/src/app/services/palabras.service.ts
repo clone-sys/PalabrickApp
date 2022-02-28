fca9c4ed4699bd64459b579815bc7cca2e8538f8
Juan Antonio Martin Lopez <juanan.martin.lpz@gmail.com>
2022-02-28 12:27:05 +0100

    FIX: Bug en servicio palabras

--- Palabrick/src/app/services/palabras.service.ts
+++ Palabrick/src/app/services/palabras.service.ts
@@ -143,7 +143,7 @@ export class PalabrasService {
   private repeticiones(pjugador: string[], presultado: number[]): number[] {
 
     // Si es un semiacierto buscamos a quien cancela. Retorna -1 si cancela a una letra o 0 si no lo hace
-    const res = presultado.map((item, index, array) => item == -1 ? this.buscarValido(pjugador[index], index) : item);
+    const res = presultado.map((item, index, array) => item == -1 ? this.buscarValido(pjugador[index], 0) : item);
 
     return res;
   }
